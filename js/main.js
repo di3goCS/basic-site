20e1d14e42e00030f9deca5543fbe95c788ddd96
di3goCS <diego.cs282@gmail.com>
2020-07-26 17:23:28 -0300

    Alteração nos comandos de mouse

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const profile_pic = document.getElementById("profile-pic");
+const section_img = document.getElementById("section-img");
+const info_box = document.getElementById("info-box");
 
 profile_pic.onclick = function() {
     let mySrc = this.getAttribute('src');
@@ -10,19 +12,14 @@ profile_pic.onclick = function() {
     }
 };
 
-const section_img = document.getElementById("section-img");
-
 section_img.onmouseover = function() {
     this.classList.add("hvr-grow");
 };
 
-
-const info_box = document.getElementById("info-box");
-
-info_box.onmouseover = function() {
+info_box.onmouseenter = function() {
     this.classList.add("hvr-grow");
 }
 
-info_box.onmouseout = function() {
+info_box.onmouseleave = function() {
     this.classList.remove("hvr-grow");
 }
