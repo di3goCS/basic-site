7b11ee34a831464154972f4b40d5f080b952684e
di3goCS <diego.cs282@gmail.com>
2020-11-08 16:52:31 -0300

    Adiciona nova seção

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const info_box = document.getElementById("info-box");
 
 profile_pic.onclick = function() {
     let mySrc = this.getAttribute('src');
-    if (mySrc == '../assets/profile_pic.png'){
-        this.setAttribute('src', '../assets/profile_pic2.jpeg');
+    if (mySrc == 'assets/profile_pic.png'){
+        this.setAttribute('src', 'assets/profile_pic2.jpeg');
     }
     else {
-        this.setAttribute('src', '../assets/profile_pic.png');
+        this.setAttribute('src', 'assets/profile_pic.png');
     }
 };
 
